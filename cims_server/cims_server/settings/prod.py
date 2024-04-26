902e3dc8d279141e66abb533ff9b6bb3191f612f
AoungY <2377289135@qq.com>
2024-04-26 20:42:20 +0800

    更新生产环境settings

--- cims_server/cims_server/settings/prod.py
+++ cims_server/cims_server/settings/prod.py
@@ -137,7 +137,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-MEDIA_STATIC_URL = 'http://47.96.104.106:8080'
+MEDIA_STATIC_URL = 'http://47.96.104.106:80'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
